c0853be7a40fd147c61c906c332d2f1f7a531694
Kloud333 <klekotvr@gmail.com>
2020-09-18 02:59:25 +0300

    - added function for getting site url by alias

--- app/src/counter.py
+++ app/src/counter.py
@@ -1,8 +1,27 @@
 import requests
+import yaml
 from bs4 import BeautifulSoup
 
 
+def get_url(url):
+    with open("app/assets/aliases.yaml", 'r') as aliases_file:
+        try:
+            aliases = yaml.safe_load(aliases_file)
+            full_url = aliases[url]
+        except yaml.YAMLError:
+            full_url = url
+        except KeyError:
+            full_url = url
+
+    if not full_url.lower().startswith("http"):
+        full_url = "https://" + full_url
+
+    return full_url
+
+
 def get_tags_numbers(url):
+    url = get_url(url)
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -12,6 +31,6 @@ def get_tags_numbers(url):
     return results
 
 
-tags_and_numbers = get_tags_numbers('https://www.google.com/')
+tags_and_numbers = get_tags_numbers('ggl')
 
 print(tags_and_numbers)
