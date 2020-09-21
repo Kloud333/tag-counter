4820e5a15a6a183b5a01ffd6a43f73e11bb1a4ca
Kloud333 <klekotvr@gmail.com>
2020-09-21 03:57:40 +0300

    - updated setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='tag-counter',
+    version='1.0',
+    description='Get HTML tags number by site url',
+    author='Volodymyr Klekot',
+    author_email='klekotVR@gmail.com',
+    url='https://github.com/Kloud333/tag-counter',
+    packages=['counter'],
+    license="MIT License",
+)
