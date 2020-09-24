be791e27c4251b61ed02d2aa913dd4eccb9ae07e
Kloud333 <klekotvr@gmail.com>
2020-09-24 03:06:28 +0300

    - created logger class

---
+++ counter/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class Logger:
+    innerLogger = logging.getLogger('logger')
+    innerLogger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler('tags_counter.log')
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s; %(message)s", "%Y-%m-%d %H:%M:%S")
+    file_handler.setFormatter(formatter)
+
+    innerLogger.addHandler(file_handler)
+
+    def info(self, domain_name: str):
+        self.innerLogger.info(domain_name)
