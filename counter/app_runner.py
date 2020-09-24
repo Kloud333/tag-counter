2f1bb59a9aed745bbdfb9e0ffe96a514eb506381
Kloud333 <klekotvr@gmail.com>
2020-09-24 03:04:33 +0300

    - created file for running different application modes

---
+++ counter/app_runner.py
@@ -0,0 +1,14 @@
+from counter.console import run_console
+from counter.gui import run_gui
+
+
+def run_append_synonym(short_name, full_name: str):
+    pass
+
+
+def run_get(url: str):
+    run_console(url)
+
+
+def run_view():
+    run_gui()
