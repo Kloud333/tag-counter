44870b802ff67fc0cee135e2bb1e47d10196ba22
Kloud333 <klekotvr@gmail.com>
2020-09-24 03:02:33 +0300

    - removed support functions

--- counter/counter.py
+++ counter/counter.py
@@ -1,76 +1,17 @@
-import requests
-import yaml
-from bs4 import BeautifulSoup
 import click
-import logging
-from pathlib import Path
-
-logging.basicConfig(filename='tags_counter.log', filemode='w', level=logging.INFO)
-
-
-def get_url(url):
-    logging.info("Open aliases yaml")
-
-    with open(str(Path(__file__).parent.absolute()) + '\\aliases.yaml', 'r') as aliases_file:
-        try:
-            logging.info("Load aliases")
-            aliases = yaml.safe_load(aliases_file)
-            full_url = aliases[url]
-            logging.info("Found alias")
-        except yaml.YAMLError:
-            logging.error("Can't parse yaml")
-            full_url = url
-        except KeyError:
-            logging.info("Can't find alias")
-            full_url = url
-
-    if not full_url.lower().startswith("http"):
-        logging.info("Add https prefix to url")
-        full_url = "https://" + full_url
-
-    return full_url
-
-
-def get_tags_numbers(url):
-    url = get_url(url)
-
-    try:
-        logging.info("Get html page")
-        response = requests.get(url)
-        logging.info("Parse html page")
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        results = {}
-        for tag in soup.findAll():
-            results[tag.name] = len(soup.findAll(tag.name))
-
-    except requests.exceptions.InvalidURL:
-        logging.error("Invalid Url")
-        return None
-    except requests.exceptions.ConnectionError:
-        logging.error("Connection error")
-        return None
-
-    return yaml.dump(results)
-
-
-def get_data(url):
-    result = get_tags_numbers(url)
-
-    if result is None:
-        print('Error: Invalid URL or Connection problems')
-        return
-
-    logging.info("Show parsing result")
-    print('Result:\n', result)
-    pass
+from counter.app_runner import run_get, run_view
 
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
-def main(get):
+@click.option('--view', '-v', is_flag=True, help='get list of tag')
+def main(get, view):
     if get is not None:
-        get_data(get)
+        run_get(get)
+    elif view is not None:
+        run_view()
+    else:
+        run_view()
 
 
 if __name__ == "__main__":
