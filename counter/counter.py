1c321d60aa7a5b573fcd2ce9e1d0d6e08ebff56c
Kloud333 <klekotvr@gmail.com>
2020-09-24 03:16:54 +0300

    - removed console parameter for running gui version

--- counter/counter.py
+++ counter/counter.py
@@ -4,12 +4,9 @@ from counter.app_runner import run_get, run_view
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
-@click.option('--view', '-v', is_flag=True, help='get list of tag')
-def main(get, view):
+def main(get):
     if get is not None:
         run_get(get)
-    elif view is not None:
-        run_view()
     else:
         run_view()
 
