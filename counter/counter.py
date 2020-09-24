5177df312ddf57c11f70eb1c523728567e39542e
Kloud333 <klekotvr@gmail.com>
2020-09-24 03:22:44 +0300

    - added parameter for loading data from db

--- counter/counter.py
+++ counter/counter.py
@@ -1,12 +1,15 @@
 import click
-from counter.app_runner import run_get, run_view
+from counter.app_runner import run_get, run_view, run_get_from_db
 
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
-def main(get):
+@click.option('--view', '-v', is_flag=True, help='get list of tag from db')
+def main(get, view):
     if get is not None:
         run_get(get)
+    elif view is not None:
+        run_get_from_db(view)
     else:
         run_view()
 
