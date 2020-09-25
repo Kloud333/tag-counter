9ef536f599d8de64ae60b762466fe5cb4e77ad56
Kloud333 <klekotvr@gmail.com>
2020-09-25 17:14:12 +0300

    - created methods for working with aliases

--- counter/aliases.py
+++ counter/aliases.py
@@ -1,20 +1,50 @@
 from pathlib import Path
 import yaml
+from counter.logger import Logger
 
 
 class Aliases:
-    @staticmethod
-    def get_url(url: str):
-        with open(str(Path(__file__).parent.absolute()) + '\\aliases.yaml', 'r') as aliases_file:
-            try:
-                aliases = yaml.safe_load(aliases_file)
-                full_url = url if '.' in url else aliases[url]
-            except yaml.YAMLError:
-                full_url = url
-            except KeyError:
-                full_url = url
+    logger = Logger()
 
-            if not full_url.lower().startswith("http"):
-                full_url = "https://" + full_url
+    def __init__(self):
+        self.path = str(Path(__file__).parent.absolute()) + '\\aliases.yaml'
+        if self.path is not None:
+            self.read(self.load_yaml)
+
+    def get_url(self, url: str):
+        full_url = url if '.' in url else self.aliases[url]
+
+        if not full_url.lower().startswith("http"):
+            full_url = "https://" + full_url
 
         return full_url
+
+    def show_aliases(self):
+        print('Alias list:\n', yaml.dump(self.aliases))
+
+    def remove_alias(self, alias):
+        del self.aliases[alias]
+        self.write(self.save)
+
+    def add_alias(self, alias, url):
+        self.aliases[alias] = url
+        self.write(self.save)
+
+    def save(self, stream):
+        yaml.dump(self.aliases, stream)
+
+    def file(self, action, mode):
+        with open(self.path, mode) as stream:
+            try:
+                action(stream)
+            except yaml.YAMLError as exc:
+                self.logger.error(exc)
+
+    def load_yaml(self, stream):
+        self.aliases = yaml.safe_load(stream)
+
+    def read(self, action):
+        self.file(action, 'r')
+
+    def write(self, action):
+        self.file(action, 'w')
