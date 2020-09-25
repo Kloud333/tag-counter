c1f3a777d8e68160aee1b7a06767eb6d753ed919
Kloud333 <klekotvr@gmail.com>
2020-09-25 17:16:23 +0300

    - added option for working with aliases file

--- counter/counter.py
+++ counter/counter.py
@@ -1,12 +1,21 @@
 import click
-from counter.app_runner import run_get, run_view
+from counter.app import run_get, run_view, show_all_aliases, remove_alias, add_alias
 
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
-def main(get):
+@click.option('--aliases', is_flag=True, help='show aliases')
+@click.option('--removealias', '-r', help='remove alias')
+@click.option('--addalias', '-a', nargs=2, help='add alias')
+def main(get, aliases, removealias, addalias):
     if get is not None:
         run_get(get)
+    elif aliases:
+        show_all_aliases()
+    elif removealias is not None:
+        remove_alias(removealias)
+    elif addalias:
+        add_alias(addalias)
     else:
         run_view()
 
