07528c043b6e9edbc0653d8240763bea4352b4ad
Kloud333 <klekotvr@gmail.com>
2020-09-25 03:45:59 +0300

    - removed view command

--- counter/counter.py
+++ counter/counter.py
@@ -1,15 +1,12 @@
 import click
-from counter.app_runner import run_get, run_view, run_get_from_db
+from counter.app_runner import run_get, run_view
 
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
-@click.option('--view', '-v', is_flag=True, help='get list of tag from db')
-def main(get, view):
+def main(get):
     if get is not None:
         run_get(get)
-    elif view is not None:
-        run_get_from_db(view)
     else:
         run_view()
 
