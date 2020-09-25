0f0de84a418443ef63ddf4c7b6914304ec41de44
Kloud333 <klekotvr@gmail.com>
2020-09-25 17:16:48 +0300

    - added error log function

--- counter/logger.py
+++ counter/logger.py
@@ -8,10 +8,17 @@ class Logger:
     file_handler = logging.FileHandler('tags_counter.log')
     file_handler.setLevel(logging.INFO)
 
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.ERROR)
+
     formatter = logging.Formatter("%(asctime)s; %(message)s", "%Y-%m-%d %H:%M:%S")
     file_handler.setFormatter(formatter)
 
     innerLogger.addHandler(file_handler)
+    innerLogger.addHandler(stream_handler)
 
-    def info(self, url: str):
-        self.innerLogger.info(url)
+    def info(self, message: str):
+        self.innerLogger.info(message)
+
+    def error(self, message: str):
+        self.innerLogger.error(message)
