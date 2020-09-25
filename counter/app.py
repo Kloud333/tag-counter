0762251e89c16ab0fda2fcf8233680bd702a371d
Kloud333 <klekotvr@gmail.com>
2020-09-25 17:13:22 +0300

    - renamed runner file

---
+++ counter/app.py
@@ -0,0 +1,25 @@
+from counter.console import console_view
+from counter.gui import gui_view
+from counter.aliases import Aliases
+
+aliases = Aliases()
+
+
+def show_all_aliases():
+    aliases.show_aliases()
+
+
+def remove_alias(alias):
+    aliases.remove_alias(alias)
+
+
+def add_alias(alias):
+    aliases.add_alias(alias[0], alias[1])
+
+
+def run_get(domain_name: str):
+    console_view(domain_name)
+
+
+def run_view():
+    gui_view()
