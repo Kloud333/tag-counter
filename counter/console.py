f0afe1e908b5ff2e6c6fbe0a6d1ee2195f7f7c1a
Kloud333 <klekotvr@gmail.com>
2020-09-25 03:45:06 +0300

    - updated console algorithm

--- counter/console.py
+++ counter/console.py
@@ -1,9 +1,13 @@
 from counter.parser import Parser
+from counter.aliases import Aliases
+from counter.storage import Storage
 
 
-def run_console(url: str):
-    parser = Parser()
-    result = parser.get_tags_numbers(url)
+def run_console_view(domain_name: str):
+    with Storage() as storage:
+        url = Aliases.get_url(domain_name)
+        parser = Parser(url, aliases=Aliases, storage=storage)
+        result = parser.count_tags()
 
     if result is None:
         print('Error: Invalid URL or Connection problems')
