2a2fcee49a909d61f85c20209345452198299f64
Kloud333 <klekotvr@gmail.com>
2020-09-25 03:50:53 +0300

    - added logger and saving to storage

--- counter/parser.py
+++ counter/parser.py
@@ -1,17 +1,19 @@
 import requests
 import yaml
-from bs4 import BeautifulSoup
-from pathlib import Path
 from counter.logger import Logger
+from bs4 import BeautifulSoup
 
 
 class Parser:
     logger = Logger()
 
-    def get_tags_numbers(self, url: str):
-        url = self.get_url(url)
+    def __init__(self, url, aliases, storage):
+        self.url = url
+        self.aliases = aliases
+        self.storage = storage
 
-        self.logger.info(url)
+    def count_tags(self):
+        url = self.aliases.get_url(self.url)
 
         try:
             response = requests.get(url)
@@ -26,20 +28,7 @@ class Parser:
         except requests.exceptions.ConnectionError:
             return None
 
+        self.logger.info(url)
+        self.storage.save_tags(url, results)
+
         return yaml.dump(results)
-
-    def get_url(self, url: str):
-
-        with open(str(Path(__file__).parent.absolute()) + '\\aliases.yaml', 'r') as aliases_file:
-            try:
-                aliases = yaml.safe_load(aliases_file)
-                full_url = aliases[url]
-            except yaml.YAMLError:
-                full_url = url
-            except KeyError:
-                full_url = url
-
-        if not full_url.lower().startswith("http"):
-            full_url = "https://" + full_url
-
-        return full_url
