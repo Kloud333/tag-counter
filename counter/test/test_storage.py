92e2c0fe2bcaa201067d61aae3048debd168e5fb
Kloud333 <klekotvr@gmail.com>
2020-10-09 16:51:49 +0300

    - updated tests for storage service

--- counter/test/test_storage.py
+++ counter/test/test_storage.py
@@ -11,20 +11,6 @@ class TestsAliases(unittest.TestCase):
 
         self.assertEqual({'html': 1, 'div': 2}, results)
 
-    def test_storage_find_url_good_response(self):
-        with Storage() as storage:
-            storage.save_tags('https://github.com', {})
-            results = storage.find_url('https://github.com')
-
-        self.assertEqual('https://github.com', results)
-
-    def test_storage_find_url_bad_response(self):
-        with Storage() as storage:
-            storage.save_tags('https://github.com', {})
-            results = storage.find_url('https://yandex.com')
-
-        self.assertEqual(None, results)
-
 
 if __name__ == '__main__':
     unittest.main()
