22cf8b58ad4c3ef8cb406584c427ba9e29c1f34b
Kloud333 <klekotvr@gmail.com>
2020-10-09 16:50:47 +0300

    - added console pharameter for get data from db

--- counter/counter.py
+++ counter/counter.py
@@ -1,15 +1,18 @@
 import click
-from counter.app import run_get, run_view, show_all_aliases, remove_alias, add_alias
+from counter.app import run_get, run_view, show_all_aliases, remove_alias, add_alias, run_gui
 
 
 @click.command()
 @click.option('--get', '-g', help='get list of tags')
+@click.option('--view', '-v', help='get data from db')
 @click.option('--aliases', is_flag=True, help='show aliases')
 @click.option('--removealias', '-r', help='remove alias')
 @click.option('--addalias', '-a', nargs=2, help='add alias')
-def main(get, aliases, removealias, addalias):
+def main(get, view, aliases, removealias, addalias):
     if get is not None:
         run_get(get)
+    elif view is not None:
+        run_view(view)
     elif aliases:
         show_all_aliases()
     elif removealias is not None:
@@ -17,7 +20,7 @@ def main(get, aliases, removealias, addalias):
     elif addalias:
         add_alias(addalias)
     else:
-        run_view()
+        run_gui()
 
 
 if __name__ == "__main__":
